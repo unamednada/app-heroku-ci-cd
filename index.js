b428c9537d36ba60f13c5a50208a8830e1d71e2f
unamednada <dias.gustavo.dias@gmail.com>
2022-02-12 10:29:04 -0300

    Start staging

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => res.send(`<h1> Executando na porta: ${port} </h1>`));
+app.get('/', (req, res) => res.send(`<h1> Executando FROM STAGING!!! na porta: ${port} </h1>`));
 app.listen(port, () => {
   console.log(`Online: ${port}`);
 });
